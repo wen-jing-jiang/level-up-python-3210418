0b4dc788e91d72dcdd49ed9c7bee024a1867f654
wen-jing-jiang <118755368+wen-jing-jiang@users.noreply.github.com>
2022-11-22 17:22:04 +0000

    finish prime factors challenge

--- src/01_Find_Prime_Factors/main.py
+++ src/01_Find_Prime_Factors/main.py
@@ -1,10 +1,14 @@
-import numpy
-
 def pfactors(num):
-  factors = []
-  primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,83,89,97]
-  for i in primes:
-    if num % primes[i] == 0:
-      factors.append(primes[i])
+    factors = []
+    primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,83,89,97]
+    for i in primes:
+        while num % i == 0:
+            factors.append(i)
+            num = num/i
+    if len(factors) == 0:
+        factors.append(num)
+    else:
+        print(factors)
 
-print(pfactors(1568))
+
+pfactors(77)
